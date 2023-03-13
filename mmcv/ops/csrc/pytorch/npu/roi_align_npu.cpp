e903c1f67908cc6eab07ee9d44084dd07a13bfcc
sherie <963372609@qq.com>
2023-03-13 19:58:13 +0800

    Update roi_align_npu.cpp

--- mmcv/ops/csrc/pytorch/npu/roi_align_npu.cpp
+++ mmcv/ops/csrc/pytorch/npu/roi_align_npu.cpp
@@ -33,7 +33,8 @@ void roi_align_backward_npu(Tensor grad_output, Tensor rois, Tensor argmax_y,
   int64_t aligned_width_64 = aligned_width;
   int64_t sampling_ratio_64 = sampling_ratio;
   int64_t roi_end_mode = 0;
-  c10::SmallVector<int64_t, SIZE> xdiff_shape = at_npu::native::array_to_small_vector(grad_input.sizes());
+  c10::SmallVector<int64_t, SIZE> xdiff_shape =
+      at_npu::native::array_to_small_vector(grad_input.sizes());
   OpCommand cmd;
   cmd.Name("ROIAlignGrad")
       .Input(grad_output)
